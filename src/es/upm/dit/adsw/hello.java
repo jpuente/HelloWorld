878cc11db59f1d947afb839a64a9e312173d9be0
Juan A. de la Puente <jpuente@dit.upm.es>
2016-11-20 17:48:31 +0100

    Initial commit

---
+++ src/es/upm/dit/adsw/hello.java
@@ -0,0 +1,12 @@
+package es.upm.dit.adsw;
+
+/**
+ * Created by jpuente on 20/11/16.
+ */
+public class hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
+
